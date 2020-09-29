cf496ddda79439560d0c336c60abd82cc2407d2a
行米希尔 <ggxmxr@126.com>
2020-09-29 10:27:23 +0800

    一堆东西

--- code-generate-server/src/main/java/xmxrProject/genServer/common/utils/C_CacheUtil.java
+++ code-generate-server/src/main/java/xmxrProject/genServer/common/utils/C_CacheUtil.java
@@ -1,8 +1,6 @@
 package xmxrProject.genServer.common.utils;
 
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * @FileName: C_CacheUtil.java
@@ -13,126 +11,115 @@ import java.util.concurrent.ConcurrentMap;
 public class C_CacheUtil {
 
     //设置初始容量
-    private static int initialCapacity = 1024;
+    private static int initialCapacity = 1024*8;
 
     private static float loadFactor = 0.75f;
 
-    private static ConcurrentHashMap<String,Value> cache = new ConcurrentHashMap<String,Value>(initialCapacity);
+    private static ConcurrentHashMap<Object, Value> cache = new ConcurrentHashMap<Object, Value>(initialCapacity);
 
     //刷新缓存计数
     private static int flushCount = 0;
 
 
     /**
-     *
      * @param key
      * @param value
      * @return
      */
-    public static Object put(String key , Object value){
-        flush();
-        Object retVal = cache.get(key).getValue();
-        cache.put(key,new Value(null,System.currentTimeMillis(),value));
-        return retVal;
+    public static Object put(Object key, Object value) {
+        return put(key, value, 0);
     }
 
     /**
-     *
-     * @param key
-     * @param outTime
-     * @return
-     */
-    public static boolean addOutTime(String key ,long outTime){
-        Value value = cache.get(key);
-        if(value == null){
-            return false;
-        }
-        value.outTime+=outTime;
-        return true;
-    }
-
-    /**
-     *
-     * @param key
-     * @param outTime
-     * @return
-     */
-    public static boolean setOutTime(String key ,long outTime){
-        Value value = cache.get(key);
-        if(value == null){
-            return false;
-        }
-        value.outTime=outTime;
-        return true;
-    }
-
-
-    /**
-     *
      * @param key
      * @param value
      * @param outTime
      * @return
      */
-    public static Object put(String key , Object value , long outTime){
+    public static Object put(Object key, Object value, long outTime) {
         flush();
-        Object retVal = cache.get(key).getValue();
-        cache.put(key,new Value(outTime*1000,System.currentTimeMillis(),value));
+        Object retVal = cache.get(key) == null ? null : cache.get(key).getValue();
+        cache.put(key, new Value(outTime * 1000, value));
         return retVal;
     }
 
     /**
-     *
+     * @param key
+     * @param outTime
+     * @return
+     */
+    public static boolean addOutTime(String key, long outTime) {
+        return cache.get(key) != null && cache.get(key).addOutTime(outTime) != null;
+    }
+
+    /**
+     * @param key
+     * @param outTime
+     * @return
+     */
+    public static boolean setOutTime(String key, long outTime) {
+        return cache.get(key) != null && cache.get(key).setOutTime(outTime) != null;
+    }
+
+
+    /**
      * @param key
      * @return
      */
-    public static Object get(String key){
-        Value value = cache.get(key);
-        if(value.isOutTime()){
-            return null;
-        }
-        return value.getValue();
+    public static Object get(String key) {
+        return cache.get(key) == null ? null : cache.get(key).getValue();
     }
+
 
     /**
      * 刷新缓存 删除已超时数据
      */
-    public static void flush(){
+    public static void flush() {
         flushCount++;
-        if(flushCount > initialCapacity*loadFactor){
-            cache.entrySet().parallelStream().filter(o->o.getValue().isOutTime()).forEach(o->{o.getValue().clear();cache.remove(o.getKey());});
+        if (flushCount > initialCapacity * loadFactor) {
+            cache.entrySet().parallelStream().filter(o -> o.getValue().isOutTime()).forEach(o -> {
+                cache.remove(o.getKey());
+            });
             flushCount = 0;
             System.gc();
         }
     }
 
 
-
-    private static class Value{
-        private Long outTime;
-        private Long saveTime;
+    private static class Value {
+        private long outTime;
+        private long saveTime;
         private Object value;
 
-        public Value(Long outTime, Long saveTime, Object value) {
+        public Value(long outTime, Object value) {
             this.outTime = outTime;
-            this.saveTime = saveTime;
+            this.saveTime = System.currentTimeMillis();
             this.value = value;
         }
 
-        private Object getValue(){
-            return  isOutTime() ? null : value;
-        }
-        private boolean isOutTime(){
-            return !(outTime == null) ||System.currentTimeMillis() - saveTime > outTime ;
+        private Object getValue() {
+            return isOutTime() ? null : value;
         }
 
-        private void clear(){
-            value = null;
-            outTime = null;
-            saveTime = null;
-
+        private boolean isOutTime() {
+            return outTime > 0 && System.currentTimeMillis() - saveTime > outTime;
         }
 
+        public Value setOutTime(long outTime) {
+            this.outTime = outTime*1000;
+            return this;
+        }
+        public Value addOutTime(long outTime) {
+            this.outTime += outTime*1000;
+            return this;
+        }
+        public Value subOutTime(long outTime) {
+            this.outTime -= outTime*1000;
+            return this;
+        }
     }
+
+
+
 
 }//end
